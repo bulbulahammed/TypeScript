8e984a9f450c8cc19118527f9cf5f2b21e880c0a
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-08-09 16:27:39 +0600

    user Defined Types

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import "./App.css";
 import User from "./components/User";
 
+const user1 = {
+  name: "Bulbul Ahammed",
+  age: 27,
+  isSmoker: false,
+  lang: ["Bengali", "English"],
+};
+const user2 = {
+  name: "Farhad Ahammed",
+  age: 22,
+  isSmoker: false,
+  lang: ["Bengali", "English"],
+};
+
 function App() {
   return (
     <div className="App">
       <h1>User Management</h1>
-      <User name="Bulbul Ahammed" age={27} isSmoker={false} />
+      <User user={user1} />
+      <User user={user2} />
     </div>
   );
 }
--- src/components/User.tsx
+++ src/components/User.tsx
@@ -1,15 +1,24 @@
 type UserProps = {
-  name: string;
-  age: number;
-  isSmoker: boolean;
+  user: {
+    name: string;
+    age: number;
+    isSmoker: boolean;
+    lang: string[];
+  };
 };
 
-const User = ({ name, age, isSmoker }: UserProps) => {
+const User = ({ user }: UserProps) => {
   return (
     <div>
-      <h2>{name}</h2>
-      <h2>{age}</h2>
-      {isSmoker ? <p>Smoker</p> : <p>Non Smoker</p>}
+      <h2>{user.name}</h2>
+      <h2>{user.age}</h2>
+      {user.isSmoker ? <p>Smoker</p> : <p>Non Smoker</p>}
+      <p>
+        Speaks:
+        {user.lang.map((language, index) => {
+          return <span key={index}>{language} </span>;
+        })}
+      </p>
     </div>
   );
 };
