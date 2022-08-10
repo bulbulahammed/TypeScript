7cc0eee4b8d870cd24a339e4a5fd921a8336ecf5
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-08-10 13:18:43 +0600

    Typing useState Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
+import { useState } from "react";
 import "./App.css";
-import Button from "./components/Button";
-import Post from "./components/Post";
-
 function App() {
+  const [count, setCount] = useState(0);
+  const [user, setUser] = useState<null | User>(null);
+
+  type User = {
+    id: number;
+    name: string;
+  };
+
+  const handleIncrement = () => {
+    setCount(count + 1);
+  };
+
+  const handleAddUser = () => {
+    setUser({ id: 1, name: "Bulbul" });
+  };
+
   return (
     <div className="App">
-      <h1>User Management</h1>
-      <Button>Click me</Button>
-      <Post />
+      <h1>Use State Hook</h1>
+      <h2>Count: {count}</h2>
+      <p>{user?.name}</p>
+      <button onClick={handleIncrement}>Increment</button>
+      <button onClick={handleAddUser}>Add User</button>
     </div>
   );
 }
