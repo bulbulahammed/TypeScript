a1e55d98148a60afcbde33751e542e95ebb0a606
Bulbul Ahammed <mdbulbulahammed@gmail.com>
2022-08-10 12:32:34 +0600

    Data fetch

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
 import "./App.css";
-import DataFetch from "./components/DataFetch";
-
-const users = [
-  {
-    id: 1,
-    name: "Bulbul Ahammed",
-    email: "mdbulbulahammed@gmail.com",
-    age: 27,
-  },
-  {
-    id: 1,
-    name: "Rakibul Islam",
-    email: "rakibulislam@gmail.com",
-    age: 20,
-  },
-];
+import Button from "./components/Button";
 
 function App() {
   return (
     <div className="App">
       <h1>User Management</h1>
-      <DataFetch status="loading" />
+      <Button>Click me</Button>
     </div>
   );
 }
